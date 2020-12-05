9e8d88c5fc7a93ec3fc12d93827647fcc514b028
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-12-05 13:44:59 -0800

    added carousel npm package

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './_main.scss';
-import Carousel from '../carousel/carousel';
-import MainArticle from '../mainarticle/mainarticle';
+// import Carousel from '../carousel/carousel';
+
+import MyCarousel from '../newscarousel/newscarousel';
+// import MainArticle from '../mainarticle/mainarticle';
 
 export default function Main() {
 	return (
 		<div className='main-grid'>
 			<div className='picture-background'>
-				<Carousel />
+				<MyCarousel />
 			</div>
-			<MainArticle />
+			{/* <MainArticle /> */}
 		</div>
 	);
 }

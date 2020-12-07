a07a7a9730e802e93fabec13a921302699e54977
Chris Bryant <chris_bryant@mac.com>
2020-12-06 20:58:50 -0800

    Added log in and sign up placeholders in progress

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import PrivateRoute from './components/ProtectedHOC/ProtectedRoute';
 import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 import { UserProvider } from './context/userContext';
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
 						</PrivateRoute>
 
 						<Route exact path='/' component={(props) => <Login {...props} />} />
+						<Route exact path='/login' component={(props) => <Login {...props} />} />
+						<Route exact path='/signup' component={(props) => <Signup {...props} />} />
 						<Route>
 							<h1>404 NOT FOUND</h1>
 						</Route>

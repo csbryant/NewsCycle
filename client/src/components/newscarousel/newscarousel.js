2d2156dd9c9a42e14842095e0f332902499838da
ooledezma <ooledezma@gmail.com>
2020-12-07 10:09:50 -0800

    added api call

--- client/src/components/newscarousel/newscarousel.js
+++ client/src/components/newscarousel/newscarousel.js
@@ -1,23 +1,48 @@
 import React, { useState } from 'react';
 import './_newscarousel.scss';
-import { staticData } from '../../utils/staticData';
+// import { staticData } from '../../utils/staticData';
+import API from "../../utils/API";
 import Carousel from 'react-elastic-carousel';
 import ItemsCarousel from '../itemscarousel/itemscarousel';
 import ActionBtn from '../buttons/actionbtn';
 import MainArticle from '../mainarticle/mainarticle';
 
-const NewsCarousel = () => {
-	return (
-		<Carousel pagination={false} className='rec rec-arrow'>
-			{staticData.map((item, index) => (
-				<div className='newsCarousel-grid' key={index}>
-					<ItemsCarousel background={item.multimedia[0].url} />
-					<ActionBtn />
-					<MainArticle title={item.title} abstract={item.abstract} />
-				</div>
-			))}
-		</Carousel>
-	);
-};
+
+
+class NewsCarousel extends React.Component {
+
+	// const [topStories, settopStories] = useState ({
+	// 	topStories:[],
+	// })
+
+	state = {
+		topStories: [],
+	}
+
+	//component did mount, fire off api call
+	componentDidMount() {
+		API.getTopStories()
+			.then(res => {
+				this.setState({
+					topStories: res.data.results,
+				});
+				console.log(res.data.results)
+			});
+	}
+
+	render() {
+		return (
+			<Carousel pagination={false} className='rec rec-arrow'>
+				{this.state.topStories.map((item, index) => (
+					<div className='newsCarousel-grid' key={index}>
+						<ItemsCarousel background={item.multimedia[0].url} />
+						<ActionBtn />
+						<MainArticle title={item.title} abstract={item.abstract} />
+					</div>
+				))}
+			</Carousel>
+		);
+	}
+}
 
 export default NewsCarousel;

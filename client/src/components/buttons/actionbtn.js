22d21466fbb3bf6dc0170cae2b1f4159e94630e7
Stuart Wong <swong31@gmail.com>
2020-12-12 13:41:28 -0800

    handleSaveArticle adds articles to favorites within GlobalState

--- client/src/components/buttons/actionbtn.js
+++ client/src/components/buttons/actionbtn.js
@@ -7,103 +7,96 @@ import { SAVE_ARTICLE, LOADING } from "../../utils/actions";
 import { useStoreContext } from "../../utils/GlobalState";
 import API from "../../utils/API";
 
-
 const ActionBtn = ({ url, handleSaveArticle }) => {
-	const [state, dispatch] = useStoreContext();
-	// const changeBackground = (e) => {
-	// 	let bg = e.target.style.backgroundColor;
-	// 	bg = { PrimaryColor };
-	// };
+  // const [state, dispatch] = useStoreContext();
+  // const changeBackground = (e) => {
+  // 	let bg = e.target.style.backgroundColor;
+  // 	bg = { PrimaryColor };
+  // };
 
-	//   useEffect(() => {
-	//     handleSaveArticle();
-	//   }, [state]);
+  //   useEffect(() => {
+  //     handleSaveArticle();
+  //   }, [state]);
 
-	function hoverHandler(e) {
-		e.target.style.background = `${PrimaryColor}`;
-	}
+  function hoverHandler(e) {
+    e.target.style.background = `${PrimaryColor}`;
+  }
 
-	function nonHoverHandler(e) {
-		e.target.style.background = `${Lightgrey}`;
-	}
+  function nonHoverHandler(e) {
+    e.target.style.background = `${Lightgrey}`;
+  }
 
+  const styles = {
+    controls: {
+      display: "flex",
+      justifyItems: "center",
+      alignItems: "center",
+    },
 
-	const styles = {
-		controls: {
-			display: 'flex',
-			justifyItems: 'center',
-			alignItems: 'center',
-		},
+    smBtn: {
+      borderRadius: "50%",
+      width: "3rem",
+      height: "3rem",
+      border: `2px solid ${Lightgrey}`,
+      opacity: `${Opacity}`,
+    },
 
+    lgBtn: {
+      fontSize: "2rem",
+      borderRadius: "50%",
+      width: "6rem",
+      height: "6rem",
+      border: `2px solid ${Lightgrey}`,
+      margin: "0 1rem",
+    },
 
-		smBtn: {
-			borderRadius: "50%",
-			width: "3rem",
-			height: "3rem",
-			border: `2px solid ${Lightgrey}`,
-			opacity: `${Opacity}`,
-		},
+    icons: {
+      fontSize: "1.5rem",
+      padding: "0.1rem",
+    },
+  };
 
+  return (
+    <>
+      <div style={styles.controls}>
+        <div>
+          <button
+            style={styles.smBtn}
+            onMouseEnter={hoverHandler}
+            onMouseLeave={nonHoverHandler}
+          >
+            <IoMdShareAlt style={styles.icons} />
+          </button>
+        </div>
 
-		lgBtn: {
-			fontSize: '2rem',
-			borderRadius: '50%',
-			width: '6rem',
-			height: '6rem',
-			border: `2px solid ${Lightgrey}`,
-			margin: '0 1rem',
-		},
+        <div>
+          <button
+            style={styles.lgBtn}
+            onMouseEnter={hoverHandler}
+            onMouseLeave={nonHoverHandler}
+            onClick={handleSaveArticle}
+            // disabled={state.loading}
+            // type = "submit"
+          >
+            <BsBookmarkFill />
+          </button>
+        </div>
 
-
-		icons: {
-			fontSize: "1.5rem",
-			padding: "0.1rem",
-		},
-	};
-
-
-	return (
-		<>
-			<div style={styles.controls}>
-				<div>
-					<button
-						style={styles.smBtn}
-						onMouseEnter={hoverHandler}
-						onMouseLeave={nonHoverHandler}
-					>
-						<IoMdShareAlt style={styles.icons} />
-					</button>
-				</div>
-
-				<div>
-					<button
-						style={styles.lgBtn}
-						onMouseEnter={hoverHandler}
-						onMouseLeave={nonHoverHandler}
-						onClick={handleSaveArticle}
-					// disabled={state.loading}
-					// type = "submit"
-					>
-						<BsBookmarkFill />
-					</button>
-				</div>
-
-				<div>
-					<a href={url}>
-						<button
-							style={styles.smBtn}
-							onMouseEnter={hoverHandler}
-							onMouseLeave={nonHoverHandler}
-						>
-							<BiLink style={styles.icons} />
-						</button>
-					</a>
-				</div>
-			</div>
-		</>
-		// </div>
-	);
-
+        <div>
+          <a href={url}>
+            <button
+              style={styles.smBtn}
+              onMouseEnter={hoverHandler}
+              onMouseLeave={nonHoverHandler}
+            >
+              <BiLink style={styles.icons} />
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+    // </div>
+  );
 };
 
 export default ActionBtn;

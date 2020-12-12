15610f525b23dadaa36e43cf9dabbc2068e92c0e
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-12-12 13:16:21 -0800

    profile link

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from "react";
-import "./_sidebar.scss";
-import ArticleList from "../../components/articlelist/articlelist";
-import { staticData } from "../../utils/staticData";
-import { useStoreContext } from "../../utils/GlobalState";
+import React, { useEffect, useState } from 'react';
+import './_sidebar.scss';
+import ArticleList from '../../components/articlelist/articlelist';
+import { staticData } from '../../utils/staticData';
+import { useStoreContext } from '../../utils/GlobalState';
 import {
-  REMOVE_FAVORITE,
-  LOADING,
-  UPDATE_FAVORITES,
-} from "../../utils/actions";
+	REMOVE_FAVORITE,
+	LOADING,
+	UPDATE_FAVORITES,
+} from '../../utils/actions';
 
 const Sidebar = () => {
-  const [state, dispatch] = useStoreContext();
+	const [state, dispatch] = useStoreContext();
 
-  const getFavorites = () => {
-    dispatch({ type: LOADING });
-    dispatch({ type: UPDATE_FAVORITES });
-  };
+	const getFavorites = () => {
+		dispatch({ type: LOADING });
+		dispatch({ type: UPDATE_FAVORITES });
+	};
 
-  const removeFromFavorites = (id) => {
-    dispatch({
-      type: REMOVE_FAVORITE,
-      payload: { _id: id },
-    });
-  };
+	const removeFromFavorites = (id) => {
+		dispatch({
+			type: REMOVE_FAVORITE,
+			payload: { _id: id },
+		});
+	};
 
-  useEffect(() => {
-    getFavorites();
-  }, []);
+	useEffect(() => {
+		getFavorites();
+	}, []);
 
-  return (
-    <header className="shadow">
-      <div className="sidenav">
-        <ul>
-          {staticData.map((article, index) => {
-            return (
-              <ArticleList
-                title={article.title}
-                url={article.url}
-                key={index}
-                backgroundImageUrl={article.multimedia[1].url}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </header>
-  );
+	return (
+		<header className='shadow'>
+			<div className='sidenav'>
+				<div>This is the link to my Profile</div>
+				<ul>
+					{staticData.map((article, index) => {
+						return (
+							<ArticleList
+								title={article.title}
+								url={article.url}
+								key={index}
+								backgroundImageUrl={article.multimedia[1].url}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+		</header>
+	);
 };
 
 export default Sidebar;

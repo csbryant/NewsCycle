61ea0d70f8871ec4ab4fd94cdb5d5aabacae93db
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-12-13 22:00:02 -0800

    logout button on sidebar

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,61 +1,64 @@
-import React, { useEffect, useState } from "react";
-import "./_sidebar.scss";
-import ArticleList from "../../components/articlelist/articlelist";
+import React, { useEffect, useState } from 'react';
+import './_sidebar.scss';
+import ArticleList from '../../components/articlelist/articlelist';
 // import { staticData } from '../../utils/staticData';
-import { useStoreContext } from "../../utils/GlobalState";
+import { useStoreContext } from '../../utils/GlobalState';
 import {
-  REMOVE_FAVORITE,
-  LOADING,
-  UPDATE_FAVORITES,
-} from "../../utils/actions";
-import API from "../../utils/API";
+	REMOVE_FAVORITE,
+	LOADING,
+	UPDATE_FAVORITES,
+} from '../../utils/actions';
+import API from '../../utils/API';
+import { Button } from 'react-bootstrap';
 
 const Sidebar = () => {
-  const [state, dispatch] = useStoreContext();
+	const [state, dispatch] = useStoreContext();
 
-  const getFavorites = () => {
-    dispatch({ type: LOADING });
-    API.getFavorites()
-      .then((result) => {
-        console.log(result);
-        dispatch({
-          type: UPDATE_FAVORITES,
-          payload: result.data.favorites,
-        });
-      })
-      .catch((err) => console.log(err));
-  };
+	const getFavorites = () => {
+		dispatch({ type: LOADING });
+		API.getFavorites()
+			.then((result) => {
+				console.log(result);
+				dispatch({
+					type: UPDATE_FAVORITES,
+					payload: result.data.favorites,
+				});
+			})
+			.catch((err) => console.log(err));
+	};
 
-  const removeFromFavorites = (id) => {
-    dispatch({
-      type: REMOVE_FAVORITE,
-      payload: { _id: id },
-    });
-  };
+	const removeFromFavorites = (id) => {
+		dispatch({
+			type: REMOVE_FAVORITE,
+			payload: { _id: id },
+		});
+	};
 
-  useEffect(() => {
-    getFavorites();
-  }, [state.favorites]);
+	useEffect(() => {
+		getFavorites();
+	}, [state.favorites]);
 
-  return (
-    <header className="shadow">
-      <div className="sidenav">
-        <div>This is the link to my Profile</div>
-        <ul>
-          {state.favorites.map((article, index) => {
-            return (
-              <ArticleList
-                title={article.title}
-                url={article.url}
-                key={index}
-                backgroundImageUrl={article.multimedia[1].url}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </header>
-  );
+	return (
+		<header className='shadow'>
+			<div className='sidenav'>
+				<Button variant='info'>
+					<a href='/'>Log Out</a>
+				</Button>
+				<ul>
+					{state.favorites.map((article, index) => {
+						return (
+							<ArticleList
+								title={article.title}
+								url={article.url}
+								key={index}
+								backgroundImageUrl={article.multimedia[1].url}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+		</header>
+	);
 };
 
 export default Sidebar;

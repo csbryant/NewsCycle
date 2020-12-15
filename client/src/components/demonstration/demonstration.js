7d917c45fc818b9388691dc0d4348219e9d385b4
Chris Bryant <chris_bryant@mac.com>
2020-12-14 17:46:58 -0800

    New Webmanifest file for all devices and new front page

--- client/src/components/demonstration/demonstration.js
+++ client/src/components/demonstration/demonstration.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Carousel } from 'react-bootstrap';
-import ipad from '../../img/ipad.png'
-import mac from '../../img/mac.png'
+import { hover } from 'react-bootstrap';
+import ipad from '../../img/newipadedit.png';
+import mac from '../../img/newmac.png';
+import iphone from '../../img/newiphone.png';
 import './_demonstration.scss';
 
 function Demo() {
 
     return (
         <>
-            <Carousel className="specific" controls={false} indicators={false}>
+            <Carousel className="specific" pause={false} controls={false} indicators={false}>
                 <Carousel.Item interval={2500}>
                     <img
                         className="d-block w-100"
@@ -20,6 +22,13 @@ function Demo() {
                     <img
                         className="d-block w-100"
                         src={ipad}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2500}>
+                    <img
+                        className="d-block w-100"
+                        src={iphone}
                         alt="Third slide"
                     />
                 </Carousel.Item>

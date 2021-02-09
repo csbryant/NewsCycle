7ce4787cfdaef4f12c1cf72bc86e2921b17538aa
ooledezma <Oscar.Ledezma@nusano.com>
2021-02-09 15:27:17 -0800

    updated apikey

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASEURL = "https://api.nytimes.com/svc/topstories/v2/us.json?api-key=";
-const APIKEY = process.env.REACT_APP_NYT_API_KEY;
+const APIKEY = BEtDrMSHqfAGbICTJYrN7RhSq3FriydD;
 
 export default {
   // Gets all users
